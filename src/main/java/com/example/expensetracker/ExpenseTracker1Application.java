71d2f5bbab556f8f8a5382ed31cc2d1534c03148
Rammoorthyk <rammoorthy.kofficial@gmail.com>
2025-12-05 20:14:08 +0530

    first commit

---
+++ src/main/java/com/example/expensetracker/ExpenseTracker1Application.java
@@ -0,0 +1,22 @@
+package com.example.expensetracker;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExpenseTracker1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTracker1Application.class, args);
+    }
+
+@GetMapping
+public String Welcome() {
+	return "maharaja is fullstack developer";
+}
+}
